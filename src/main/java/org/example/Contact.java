4cf9e6ca1a5a283deddb009c19c4d3ef777b46e6
unknown <nguyenducbaokey@gmail.com>
2023-04-27 15:15:16 +0700

    Finish

---
+++ src/main/java/org/example/Contact.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Contact {
+    private int contact_id;
+    private String name;
+    private String email;
+    private String address;
+    private String telephone;
+
+    @Override
+    public String toString() {
+        return String.format("[%d - %s - %s - %s -%s]", contact_id, name, email, address, telephone);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getContact_id() {
+        return contact_id;
+    }
+
+    public void setContact_id(int contact_id) {
+        this.contact_id = contact_id;
+    }
+}
